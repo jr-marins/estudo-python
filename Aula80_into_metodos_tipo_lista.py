4d5a7e31d94177245eb1718df4686fc7947c87c5
Marcos Mr <marcosmarinsjr@gmail.com>
2024-07-30 22:06:38 -0300

    Métodos básicos de listas

---
+++ Aula80_into_metodos_tipo_lista.py
@@ -0,0 +1,88 @@
+'''
+Listas em python
+
+Tipo lista - Mutável
+Suporta varios valores
+Conhecimentos reutilizáveis - índices e fatiamento
+
+Métodos úteis em lista :
+> append = adiciona um item no final da lista
+> insert = Adiciona um ítem no índice escolhido
+> pop = Remove o último ítem da lisita ou o índice escolhido e retorna o valor
+> del = apaga um índice
+> clear = limpa a lista
+> extend = estende a lista + concatena listas
+> e etc ...
+
+'''
+
+# Criação de uma lista
+minha_lista = ["dados", "código", "lógica"]
+
+# Usando conhecimento que já vimos = acessando por índice
+print(minha_lista[2])
+# >> "lógica"
+
+print()
+
+# Alterando ítem pelo índice
+minha_lista[2] = "Lógica de programação"
+print(minha_lista)
+# >> ['dados', 'código', 'Lógica de programação']
+
+print()
+
+# Pegando um valor da lista pelo índice e jogando dentro de uma variável
+base = minha_lista[0]
+print(base)
+# >> Dados
+# OBS: o valor enviado para a variável, continua existindo na minha_lista
+
+print()
+
+# append = adicionando novo ítem
+minha_lista.append("python")
+print(minha_lista)
+# >> ['dados', 'código', 'Lógica de programação', 'python']
+
+print()
+
+# pop = removendo um ítem
+# minha_lista.pop()
+
+# Podemos ver o valor que foi removido guardando-o em uma variável
+removido = minha_lista.pop()
+print(minha_lista, "Removido: ", removido)
+# >> ['dados', 'código', 'Lógica de programação'] Removido:  python
+
+# Com pop podemos também remover multiplos ítens por vez QUANDO FOR UMA LISTA PEQUENA
+# EX minha_lista.pop([])
+
+# Del = deleta um ítem
+minha_lista.append(123)
+print("\n", minha_lista)
+# >> ['dados', 'código', 'Lógica de programação', 123]
+
+del minha_lista[-1] # Removendo o último ítem por meio de um índice invertido
+print("\n", minha_lista)
+# >> ['dados', 'código', 'Lógica de programação']
+
+# Insert = adicionando um ítem em um índice escolhido.
+minha_lista.insert(0, "Base de Dados")
+# O insert recebe dois parametros, um se refere ao local em que você irá alterar o outro é o valor que você vai adicionar
+print("\n", minha_lista)
+# >>  ['Base de Dados', 'dados', 'código', 'Lógica de programação']
+
+# Extend = 
+lista_a = ["Developer", "Software"]
+lista_a.extend(minha_lista)
+print("\n", lista_a)
+# >> ['Developer', 'Software', 'Base de Dados', 'dados', 'código', 'Lógica de programação']
+# o extend não retorna nada, somente altera a lista silenciosamente.
+
+# Conacatenando = lista + lista
+lista_b = ["Eu sou"]
+lista_mae = lista_b + lista_a
+print("\n", lista_mae)
+# >>  ['Eu sou', 'Developer', 'Software', 'Base de Dados', 'dados', 'código', 'Lógica de programação']
+
