789f8d716a07549921dcc6f9dceb893fcfa36fb6
Marcos Mr <marcosmarinsjr@gmail.com>
2024-08-15 12:53:45 -0300

    Inserindo comentários para melhor etendimento.

--- Aula70_while_iterando_string.py
+++ Aula70_while_iterando_string.py
@@ -6,29 +6,42 @@ Assim, retornando a que mais aparece.
 Para isso usaremos um método chamado de Count
 '''
 
-frase = "Python é uma linguagem de programação"\
-"multiparadigma"\
-"python foi crido por Guido van Rossun."
+# primeiro cria a frase
+frase = input("Insira uma frase: \n")
 
+#criando o controlador
 i = 0
 
-qnt_apareceu_mais_vezes = 0
-letra_apareceu_mais_vezes = ""
+# as variaveis que armazenaram o valo final
+qnt_da_letra_atual = 0
+letra_que_mais_teve_ocorrencia_na_frase = ''
 
-while i < len(frase):
+# enquanto o i for menor que o tamanho da frase
+while i < len(frase): 
 
+# pegando a letra atual em que o python está neste momento.
     letra_atual = frase[i]
 
-    if letra_atual == " " :
-        i += 1
+# Ignorando espaços
+    if letra_atual == " ":
+        i+=1
         continue
+    
+# criando uma variavel que conte a ocorrencia da letra atual na frase toda
+    qnt_letra_atual_na_frase = frase.count(letra_atual)
 
-    qnt_atual = frase.count(letra_atual)
+# trabalhando os dados
+    # o if faz a comparação com a qnt atual que está fora do loop, que neste caso aindo é 0, e o loop ja começou sendo assim a qnt letra atual na frase é maior que a qnt atual 0, e quando o python voltar neste trecho de código ele irá comparar a qnt da letra atual na frase com a quantidade atual que foi armazenado do loop anterior. se a qnt atual(que ele armezenou do ultimo loop) for menor que a qnt da letra atual na frase(do loop atual), ele ira entrar no bloco if para alterar os valores das variáveis.
 
-    if qnt_apareceu_mais_vezes <= qnt_atual :
-        qnt_apareceu_mais_vezes = qnt_atual # guardando a qnt atual dentro da variavel
-        letra_apareceu_mais_vezes = letra_atual # guardando a letra atual se for a que mais aparece
-    i += 1
+    if qnt_da_letra_atual <= qnt_letra_atual_na_frase:
 
-print(f"a letra que mais apareceu foi : {letra_apareceu_mais_vezes}, e apareceu > {qnt_apareceu_mais_vezes} vezes ")
-print(letra_apareceu_mais_vezes)
+        # sendo a qnt atual menor que a quantidade da letra atual da frase, a qnt atual passa a ter o valor da qnt da letra atual na frase, pois a letra atual aparece mais que a anterior
+        qnt_da_letra_atual = qnt_letra_atual_na_frase
+        
+        # a letra que será armazenada na variavel (letra que mais teve ocorrencia), é a letra que deu a origem a ultima modificação do valor da variavel qnt atual. 
+        letra_que_mais_teve_ocorrencia_na_frase = letra_atual
+    i+=1
+
+print(
+    f"A letra que mais apareceu foi ({letra_que_mais_teve_ocorrencia_na_frase.upper()}) e aparece : ({qnt_da_letra_atual})X"
+    )
