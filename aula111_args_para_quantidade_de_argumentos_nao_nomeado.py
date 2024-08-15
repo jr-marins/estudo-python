ae3af11e409ba59e71583661b262827d39bfdf0d
Marcos Mr <marcosmarinsjr@gmail.com>
2024-08-15 20:32:08 -0300

    Inserindo o método sum para iteráveis

--- aula111_args_para_quantidade_de_argumentos_nao_nomeado.py
+++ aula111_args_para_quantidade_de_argumentos_nao_nomeado.py
@@ -4,6 +4,7 @@ args - argumentos não nomeados
 * - *args (empacotamento e desempacotamento)
 '''
 
+# args empacota dentro de uma tupla oque eu enviar para a minha função.
 def soma(*args):
     # crio um controlador, para armazenar os valores que são iterados
     total = 0 
@@ -26,5 +27,15 @@ total1 = soma(1, 2, 3, 4, 5, 6)
 
 # acessando o valor armazenado na variável. 
 print(total1)
+# >> 21
 
+# Exemplificando a função que crie
+'''
+No python temos um método usado para numeros ou iteráveis.
+que é Sum() e recebe uma tupla
+'''
 
+print()
+print(sum((1, 2, 3, 4, 5, 6)))
+# >> 21
+# O valor é o mesmo e é bem mais simples do que construir uma função.
