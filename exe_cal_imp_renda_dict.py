5a674dd39dc0ebc58dfed2e52634f6a260feb8c5
Marcos Mr <marcosmarinsjr@gmail.com>
2024-09-08 18:59:43 -0300

    Inserindo função que compare os salários

--- exe_cal_imp_renda_dict.py
+++ exe_cal_imp_renda_dict.py
@@ -12,6 +12,20 @@ cargo_salario = {
     "estagiario": 800.0,
 }
 
+# função que retorna o cargo com o maior salário
+def encontrar_maior_salario(cargo_salario):
+    maior_salario = 0  # Variável para armazenar o maior salário
+    cargo_maior_salario = ""  # Variável para armazenar o cargo com maior salário
+
+    # Percorrendo o dicionário
+    for cargo, salario in cargo_salario.items():
+        if salario > maior_salario:  # Se o salário atual for maior que o maior salário
+            maior_salario = salario  # Atualiza o maior salário
+            cargo_maior_salario = cargo  # Atualiza o cargo correspondente
+
+    return cargo_maior_salario, maior_salario
+
+
 # crianod o dict que vai guarda os valores do impostos
 imposto_por_cargo = {}
 
@@ -25,7 +39,11 @@ for cargo, salario in cargo_salario.items():
     imposto = cal_imposto(salario)
     imposto_por_cargo[cargo] = imposto
 
-print("##impostos pagados por cargos##".upper())
+print("##impostos pagados por cargos## \n".upper())
 for cargo, imposto in imposto_por_cargo.items():
     print(f"{cargo} - imposto pago: R$ {imposto:.2f}")
+
+# Encontrar o cargo com o maior salário
+cargo, salario = encontrar_maior_salario(cargo_salario)
+print(f"\nO cargo com o maior salário é '{cargo}' com salário de R$ {salario:.2f}")
 
