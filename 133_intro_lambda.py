0287743fc8bfcae9266dd1bbee05ad5d920e3628
Marcos Mr <marcosmarinsjr@gmail.com>
2024-12-03 02:16:50 -0300

    funcao que executa a lambda

--- 133_intro_lambda.py
+++ 133_intro_lambda.py
@@ -7,17 +7,22 @@ Ou seja tudo deve ser contido dentro de uma única expressão.
 
 lista = [
     {'nome': 'Marcos', 'sobrenome': 'Marins'},
-    {'nome': 'Marco', 'sobrenome': 'Marins'},
+    {'nome': 'ana', 'sobrenome': 'Marins'},
     {'nome': 'emanuel', 'sobrenome': 'Marinss'},
     {'nome': 'joao', 'sobrenome': 'junior'},
 ]
 
-# vamos criar um filtro usando lambda
-def executa(funcao, *args):
-    return funcao, *args
-
-
 # lista.sort(key=lambda item: item) mexe diretamente na lista
 l1 = sorted(lista, key=lambda item: item['nome']) # faz uma copia da lista original
+for item in l1:
+    print(item)
 
-print(l1)
+# funçãço que recebe uma função e a executa
+def executa(func, *args):
+    return func(*args)
+
+print(
+    executa(
+        lambda x, y: x + y, 10, 5
+    )
+)
