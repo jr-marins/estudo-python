1f2f8178dc98eb94f533a64d540cd19a31d32f8c
Marcos Mr <marcosmarinsjr@gmail.com>
2024-12-19 00:02:48 -0300

    Inserindo excessões nomeadas e finally

--- 149_tryExceptFinally.py
+++ 149_tryExceptFinally.py
@@ -15,3 +15,66 @@ except TypeError as erro:
 
 except NameError as erro:
     print("parametro(variável) não definida.\n", erro)
+
+######################### Parte 2 ##############################
+
+d= 18
+e= 0
+f = 'a'
+
+'''
+tratando erros nomeados e a Exception que engloba qualquer erro em python.
+'''
+
+try:
+    divide = d / e
+    maisStr = e - f
+except ZeroDivisionError as erro:
+    print("cheguei aqui\n", erro)
+
+except (TypeError, IndexError) as erro:
+    print("TypeError + IndexError:\n",erro)
+
+    # para ver o nome do erro...
+    print("Nome: ", erro.__class__.__name__)
+
+except NameError as erro:
+    print("parametro(variável) não definida.\n", erro)
+
+except Exception:
+    print("Erro desconhecido.")
+
+############ Parte 3 ############
+
+# >> finally : bloco que sempre será executado, sempre.
+# bloco finally para fializar o try except. 
+"""
+Ex: 
+ao processar um aquivo, caso ao abri-lo dê um erro, vc deve fecha-lo. o finally é responsável por essa operação. 
+"""
+
+g = 18
+h = 0
+i = 'a'
+
+try:
+    # divide = g / h
+    # maisStr = g - i
+    # na = b + l
+except ZeroDivisionError as erro:
+    print("cheguei aqui\n", erro)
+
+except (TypeError, IndexError) as erro:
+    print("TypeError + IndexError:\n",erro)
+
+    # para ver o nome do erro...
+    print("Nome: ", erro.__class__.__name__)
+
+except NameError as erro:
+    print("parametro(variável) não definida.\n", erro)
+
+except Exception:
+    print("Erro desconhecido.")
+
+finally:
+    print("Sempre vou ser executado!")
