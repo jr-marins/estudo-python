217c7acc3de39cfcf6752754f81ce0a34b98bf05
Marcos Mr <marcosmarinsjr@gmail.com>
2025-01-03 23:33:54 -0300

    Funções decoradoras.

---
+++ aula_166DecoradoresSintaxeSugar.py
@@ -0,0 +1,28 @@
+
+# função que recebe uma função
+def criaFuncao(func):
+    # função interna que recebe argumentos
+    def interna(*args, **kwargs):
+        print("Vou decorar")
+        for arg in args:
+            # a função ESTRING recebe os argumentos
+            estring(arg)
+
+        # o resultado é a função passada como parâmetro e os argumentos são passados para a função que foi passada como parâmetro.
+        resultado = func(*args, **kwargs)
+
+        print(f"O seu resultado foi {resultado}.")
+        print("Ok. agora vc foi decorada")
+        return resultado
+    return interna
+
+@criaFuncao
+def invertString(string):
+    return string[::-1]
+
+def estring(param):
+    if not isinstance(param, str):
+        raise TypeError("param deve ser uma string")
+    
+invertida = invertString("abc")
+print(invertida)
