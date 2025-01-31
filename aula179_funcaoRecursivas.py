f0e3443095a128896c17e878de7e2edcba9a9b70
Marcos Mr <marcosmarinsjr@gmail.com>
2025-01-31 12:40:24 -0300

    funções recirsivas.Chamam a si mesmas.

---
+++ aula179_funcaoRecursivas.py
@@ -0,0 +1,25 @@
+'''
+Funções recursivas e recursividade
+
+- funções que podem se chamar de volta,
+útel p/ dividir problemas grandes em partes menores.
+- Um caso recursivo que resolve o pequeno problema
+- Um caso base que para a recursão
+
+# fatorial - n! = 5! = 5 * 4 * 3 * 2 * 1 = 120
+
+'''
+
+def recursiva(inicio=0, fim=10):
+    # caso base
+    if inicio >= fim:
+        return fim
+    
+    print(inicio, fim, sep='-')
+    # Caso recursivo
+    # contar até chegar ao fim
+
+    inicio += 1 
+    return recursiva(inicio, fim)
+
+print(recursiva())
