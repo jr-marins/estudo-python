8512082f223763d1bd5fad59a79827096dcb9e14
Marcos Mr <marcosmarinsjr@gmail.com>
2025-01-31 12:46:38 -0300

    resolvendo um problea com uma funcao recursiva

--- aula179_funcaoRecursivas.py
+++ aula179_funcaoRecursivas.py
@@ -22,4 +22,13 @@ def recursiva(inicio=0, fim=10):
     inicio += 1 
     return recursiva(inicio, fim)
 
-print(recursiva())
+# print(recursiva())
+
+# recursiva resolvendo um problema
+
+def factorial(n):
+    if n <= 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5))
